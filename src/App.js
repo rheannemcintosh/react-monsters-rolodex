b9eb15dba4ab35e94fc1f14f6821a3d804f8dfad
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-11 23:45:10 +0100

    Update the Tab size for App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor () {
-    super();
-    this.state = {
-      monsters: [
-        {
-          id: 'F1',
-          name: 'Frankenstein'
-        },
-        {
-          id: 'D1',
-          name: 'Dracula'
-        },
-        {
-          id: 'Z1',
-          name: 'Zombie'
-        },
-      ]
-    };
-  }
+	constructor () {
+		super();
+		this.state = {
+			monsters: [
+				{
+					id: 'F1',
+					name: 'Frankenstein'
+				},
+				{
+					id: 'D1',
+					name: 'Dracula'
+				},
+				{
+					id: 'Z1',
+					name: 'Zombie'
+				},
+			]
+		};
+	}
 
-  render () {
-    return (
-      <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={ monster.id }> { monster.name } </h1>
-        ))}
-      </div>
-    );
-  }
+	render () {
+		return (
+			<div className="App">
+				{this.state.monsters.map(monster => (
+					<h1 key={ monster.id }> { monster.name } </h1>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
