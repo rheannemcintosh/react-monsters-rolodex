55f40773f3b945240d0142bf3a3d9c4c9ccbfd08
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-04-11 23:43:56 +0100

    Set state with monsters and display these

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import './App.css';
 class App extends Component {
   constructor () {
     super();
+    this.state = {
+      monsters: [
+        {
+          id: 'F1',
+          name: 'Frankenstein'
+        },
+        {
+          id: 'D1',
+          name: 'Dracula'
+        },
+        {
+          id: 'Z1',
+          name: 'Zombie'
+        },
+      ]
+    };
   }
 
   render () {
     return (
       <div className="App">
+        {this.state.monsters.map(monster => (
+          <h1 key={ monster.id }> { monster.name } </h1>
+        ))}
       </div>
     );
   }
